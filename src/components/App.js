c4a50067eeddd112d4858d809cca45a0b7b8e610
Jason L Perry <jasper@ambethia.com>
2017-08-02 19:19:08 -0400

    add html stuffs

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,32 @@ import React, { Component } from 'react'
 class App extends Component {
   render () {
     return (
-      <div>
-        <h1>Hello, World!</h1>
-      </div>
+      <main>
+        <h1>A Color Picker</h1>
+        <div className='color-well'>
+          <figure>
+            <figcaption>hsl(0, 0, 0)</figcaption>
+          </figure>
+        </div>
+        <ul className='sliders'>
+          <li>
+            <label>Hue</label>
+            <input type='range' name='hue' min='0' max='360' />
+          </li>
+          <li>
+            <label>Saturation</label>
+            <input type='range' name='saturation' />
+          </li>
+          <li>
+            <label>Lightness</label>
+            <input type='range' name='lightness' />
+          </li>
+          <li>
+            <label>Alpha</label>
+            <input type='range' name='alpha' min='0' max='1' step='0.01' />
+          </li>
+        </ul>
+      </main>
     )
   }
 }
